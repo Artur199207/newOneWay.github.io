a6e40ec5d5ba94437ede5a47f357ddc59468baab
Artur199207 <artcomputers92@gmail.com>
2023-12-28 14:48:19 +0400

    settings

---
+++ script-js/login.js
@@ -0,0 +1,12 @@
+
+  const element = document.getElementById("chng");
+ 
+element.addEventListener('click',function(){
+    window.location.href = "changepassword.html";
+})
+element.style.cursor = 'pointer';
+
+
+
+
+
