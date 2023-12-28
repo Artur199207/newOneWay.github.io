03e04ebdf1b4764858323f4436f8bed06fcfd594
Artur199207 <artcomputers92@gmail.com>
2023-12-28 16:26:29 +0400

    newwww

---
+++ script-js/passwordChange.js
@@ -0,0 +1,4 @@
+const pass = document.querySelector('.content-pass-none span');
+pass.addEventListener('click',function(){
+    window.location.href = 'changepassword.html'
+})
--- script-js/settings.js
+++ script-js/settings.js
@@ -2,4 +2,8 @@ const changeSettings = document.querySelector('.settings-text span');
 changeSettings.addEventListener('click', function(){
     window.location.href = 'changeSettings.html'
 })
-changeSettings.style.cursor = 'pointer'
+changeSettings.style.cursor = 'pointer';
+const passChange = document.getElementById('change');
+passChange.addEventListener('click',function(){
+    window.location.href = 'passwordChange.html'
+})
