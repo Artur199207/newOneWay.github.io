54300c2d5656ca50d6dd32deea41ed854b1b3593
Artur199207 <artcomputers92@gmail.com>
2023-12-28 15:42:11 +0400

    new page

---
+++ script-js/settings.js
@@ -0,0 +1,5 @@
+const changeSettings = document.querySelector('.settings-text span');
+changeSettings.addEventListener('click', function(){
+    window.location.href = 'changeSettings.html'
+})
+changeSettings.style.cursor = 'pointer'
