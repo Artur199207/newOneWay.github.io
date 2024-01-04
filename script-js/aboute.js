0797fd62d46c49bf92526fdb06e41151f92606f4
Artur199207 <artcomputers92@gmail.com>
2024-01-04 15:09:03 +0400

    new

---
+++ script-js/aboute.js
@@ -0,0 +1,10 @@
+
+
+
+function openOverlay() {
+    document.getElementById("overlay").style.display = "block";
+  }
+
+  function closeOverlay() {
+    document.getElementById("overlay").style.display = "none";
+  }
