bdaea8cc7a9ed34a6b0c105e4cbb2c2242f4c4ff
Artur199207 <artcomputers*92@gmail.com>
2024-03-11 22:56:50 +0400

    gagoyo

--- script-js/aboute.js
+++ script-js/aboute.js
@@ -1,20 +1,23 @@
-
-
-
 function openOverlay() {
-    document.getElementById("overlay").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closeOverlay() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+let myDiv = document.querySelector(".openMenu");
+let container = document.querySelector(".downDv");
+let buttOP = document.getElementById("buttOp");
+
+function openMenu() {
+    myDiv.style.display = myDiv.style.display === "block" ? "none" : "block";
+    buttOP.style.transform = buttOP.style.transform ==="rotate(180deg)" ? "rotate(0deg)" : "rotate(180deg)";
+}
+
+window.addEventListener("click", function (event) {
+  if(!container.contains(event.target) && myDiv.style.display === "block") {
+    myDiv.style.display = "none";
+    buttOP.style.transform = "rotate(0deg)";
   }
-
-  function closeOverlay() {
-    document.getElementById("overlay").style.display = "none";
-  }
-
-  function openMenu(){
-    const menuOpen = document.querySelector(".openMenu");
-    menuOpen.style.display="flex";
-    document.getElementById('buttOp').style.transform = 'rotate(180deg)';
-  }
-
-
-
-  
+})
